691f36d2d784d691f46411eb105ecd63016f43be
John Hunter <john@pushagency.io>
2018-04-13 14:10:34 -0400

    first graph class

---
+++ src/main/java/Graph.java
@@ -0,0 +1,26 @@
+/**
+ * Created by John on 4/13/18.
+ */
+public class Graph {
+
+    private final int V;
+    private Bag<Integer>[] adj;
+
+    public Graph(int V){
+        this.V = V;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for(int v = 0; v < V; v++){
+            adj[v] = new Bag<>();
+        }
+    }
+
+    public void addEdge(int v, int w){
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    public Iterable<Integer> adj(int v){
+        return adj[v];
+    }
+
+}
