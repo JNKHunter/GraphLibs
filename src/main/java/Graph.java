5abb749c55f69f3249435136638e2742e2e6e8be
John Hunter <john@pushagency.io>
2018-04-20 11:11:32 -0400

    added more tests

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * Created by John on 4/13/18.
  */
@@ -14,17 +16,27 @@ public class Graph {
         }
     }
 
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
     public void addEdge(int v, int w){
+        validateVertex(v);
+        validateVertex(w);
         adj[v].add(w);
         adj[w].add(v);
     }
 
     public Iterable<Integer> adj(int v){
+        validateVertex(v);
         return adj[v];
     }
 
     public int getNumVertices() {
         return V;
     }
+
+
 
 }
--- src/test/java/CCTest.java
+++ src/test/java/CCTest.java
@@ -1,19 +1,84 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by John on 4/19/18.
  */
 public class CCTest {
-    private Graph graph;
+    private Graph autoGraph;
+    private Graph manualGraph;
     private int sideOfSquare = 3;
 
     @Before
     public void setUp() throws Exception {
-        graph = new Graph((sideOfSquare*sideOfSquare) + 2); //9 * 9 board with 2 dummy vertices
+        autoGraph = setupBoardAutomated(sideOfSquare);
+        manualGraph = setup2Components(sideOfSquare);
+    }
 
+    @Test
+    public void testGridAutomated() throws Exception {
+        CC cc = new CC(autoGraph);
+        assertEquals(1, cc.getNumberOfComponents());
+    }
+
+    @Test
+    public void test2ComponentGrid() throws Exception {
+
+        CC cc = new CC(manualGraph);
+        assertEquals(2, cc.getNumberOfComponents());
+        
+    }
+
+    //This board sets up 
+    private Graph setup2Components(int sideOfSquare){
+        Graph graph = new Graph((sideOfSquare*sideOfSquare) + 2); //9 * 9 board with 2 dummy vertices
+
+        //setup array
+        //(List<Integer>[])
+        List<Integer>[] edgeList = new ArrayList[graph.getNumVertices()];
+        for(int i = 0; i < graph.getNumVertices(); i++) {
+            edgeList[i] = new ArrayList<>();
+        }
+
+        //Create first component
+        edgeList[1].add(2);
+        edgeList[2].add(3);
+
+        //Create second component
+        edgeList[4].add(5);
+        edgeList[4].add(7);
+        edgeList[5].add(6);
+        edgeList[5].add(8);
+        edgeList[6].add(9);
+
+        edgeList[7].add(8);
+        edgeList[8].add(9);
+
+        //Connect dummy vertices to top and bottom rows
+        edgeList[0].add(1);
+        edgeList[0].add(2);
+        edgeList[0].add(3);
+
+        edgeList[7].add(10);
+        edgeList[8].add(10);
+        edgeList[9].add(10);
+
+        for(int i = 0; i < edgeList.length; i++){
+            for(Integer vertex : edgeList[i]) {
+                graph.addEdge(i, vertex);
+            }
+        }
+        return graph;
+    }
+    
+
+    private Graph setupBoardAutomated(int sideOfSquare) {
+        Graph graph = new Graph((sideOfSquare*sideOfSquare) + 2); //9 * 9 board with 2 dummy vertices
         for(int i = 1; i < graph.getNumVertices() - 1; i++){
             //if last column, don't add right connection
             if(i % 3 != 0){
@@ -34,17 +99,6 @@ public class CCTest {
         for(int k = lastRow; k < lastRow + sideOfSquare; k++){
             graph.addEdge(graph.getNumVertices() - 1, k);
         }
-
-        int sfs = 0;
-
+        return graph;
     }
-
-    @Test
-    public void testGrid() throws Exception {
-        CC cc = new CC(graph);
-        assertEquals(1, cc.getNumberOfComponents());
-    }
-
-
-
 }
